0fefca395b30467ca21f45e4d20496ccec5027a1
mateo-kova <mateo-kova0510@gmail.com>
2021-09-17 11:37:24 -0400

    lesson1

--- Contract.sol
+++ Contract.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
     uint dnaDigits = 16;
-    uint dnaModules = 10**dnaDigits;
+    uint dnaModulus = 10**dnaDigits;
     struct Zombie {
         string name;
         uint dna;
@@ -14,6 +14,11 @@ contract ZombieFactory {
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
-
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 }
